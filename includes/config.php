1d91ab0f6a8907397c676e1ab20e247e98654439
Patient Assontia <assontia@gmail.com>
2022-10-28 02:48:44 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 //define('DB_HOST', 'localhost');
-define('DB_NAME', 'exportfoot_33');
-define('DB_USER','exportfoot_patient');
-define('DB_PASSWORD','30Decembre?');
+define('DB_NAME', 'xxxxxxx3');
+define('DB_USER','xxxxxx');
+define('DB_PASSWORD','xxxxxxxx');
 
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
